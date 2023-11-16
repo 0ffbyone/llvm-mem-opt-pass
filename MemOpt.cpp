58dc225e3f498cbce063e6e42f13eae38c394ee2
ermakov anton <ermakov.ao@phystech.edu>
2023-11-17 01:01:24 +0300

    получил вес каждой ветки

--- MemOpt.cpp
+++ MemOpt.cpp
@@ -34,6 +34,28 @@ CallInst* findHeapAllocation(Function& func) {
 }
 
 
+uint64_t branchWeight(const BranchInst& branchInst) {
+    //MDNode* metaData = branchInst.getMetadata("prof");
+    MDNode* metaData = branchInst.getMetadata(LLVMContext::MD_prof);
+
+    if (branchInst.isConditional() && metaData) {
+        uint64_t weight = 0;
+        //branchInst.extractProfTotalWeight(weight);
+        //const MDOperand& wtf = metaData->getOperand(0);
+        //auto firstVal = wtf->print();
+        //auto wtf = metaData->
+        //errs() << "num of operands " << wtf  << '\n';
+        metaData->getOperand(0)->print(errs());
+        errs() << '\n';
+        metaData->getOperand(1)->print(errs());
+        errs() << '\n';
+        metaData->getOperand(2)->print(errs());
+        errs() << '\n';
+        return weight;
+    }
+    return 0;
+}
+
 BranchInst* findWeightedBranch(Function& func) {
     for (BasicBlock& block: func) {
         for (Instruction& inst : block) {
@@ -41,23 +63,17 @@ BranchInst* findWeightedBranch(Function& func) {
 
             // Только br инструкции
             if (branchInst) {
-                MDNode* metaData = branchInst->getMetadata("prof");
-
-                // Только br иструкции, которые потенциально могут быть с весом
-                if (branchInst->isConditional() && metaData) {
-                    uint64_t branchWeight = 0;
-                    branchInst->extractProfTotalWeight(branchWeight);
-                    if (branchWeight >= 2000) {
-                        return branchInst;
-                        //branchInst->getSuccessor(0);
+                uint64_t weight = branchWeight(*branchInst);
+                if (weight >= 2000) {
+                    errs() <<  "in branch: "<< weight << '\n';
+                    return branchInst;
+                    //branchInst->getSuccessor(0);
                     }
                 }
             }
         }
-    }
     return nullptr;
 }
-
 
 bool blockAccessAllocMemory(BasicBlock* block, CallInst* alloc) {
     if (block == nullptr or alloc == nullptr) {
@@ -78,13 +94,22 @@ bool blockAccessAllocMemory(BasicBlock* block, CallInst* alloc) {
 
 
 
-bool needOptimization(BranchInst* weightedBranch, CallInst* alloc) {
 
+bool needOptimization(BranchInst* weightedBranch, CallInst* alloc) {
+    if (weightedBranch == nullptr or alloc == nullptr) {
+        return false;
+    }
+    size_t numSuccessors = weightedBranch->getNumSuccessors();
+
+    for (size_t i = 0; i < numSuccessors; ++i) {
+        auto currBlock = weightedBranch->getSuccessor(i);
+    }
+    return false;
 }
 
 
 // пройтись по всем бренчам, найти именно ту, которая является unlikely
-// и еслив ней я буду использовать память и нигде больше, 
+// и еслив ней я буду использовать память и нигде больше,
 // то тогда Transformation pass
 
 // New PM implementation
@@ -97,9 +122,9 @@ struct MemOpt : PassInfoMixin<MemOpt> {
 
 
 
-    if (blockAccessAllocMemory(weightedBranch->getSuccessor(0), alloc)) {
-        errs() << "УРАААА\n";
-    }
+    //if (blockAccessAllocMemory(weightedBranch->getSuccessor(0), alloc)) {
+    //    errs() << "УРАААА\n";
+    //}
 
 
 
