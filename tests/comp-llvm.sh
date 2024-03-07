ce7373f8f94a898f2ca67208443b0985110a36b2
ermakov anton <ermakov.ao@phystech.edu>
2024-03-07 15:10:13 +0300

    в /tests/comp-llvm.sh добавлена опция генерирования бинарного файла из оптимизированного IR

--- tests/comp-llvm.sh
+++ tests/comp-llvm.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-NAME="$(basename "${1}" .c)"
+if [ $# -eq 1 ]; then
+    NAME="$(basename "${1}" .c)"
 
-# С уровнем оптимизации -O0 атрибут "branch_weights" не генерируется,
-# иногда и с -O1 атрибут "branch_weights", это зависит от кода программы
-# эти вещи нужно проверять вручную
-clang -emit-llvm -S -O1 "${1}" -o ./IR/"${NAME}".ll
+    # С уровнем оптимизации -O0 атрибут "branch_weights" не генерируется,
+    # иногда и с -O1 атрибут "branch_weights", это зависит от кода программы
+    # эти вещи нужно проверять вручную
+    clang -emit-llvm -S -O1 "${1}" -o ./IR/"${NAME}".ll
+
+elif [ $# -eq 2 ] && [ "${1}" = "bin" ]; then
+    NAME="$(basename "${2}" .c)"
+
+    llc -filetype=obj ./IROptimized/"${NAME}".ll -o ./bin/"${NAME}".o
+    clang ./bin/main.o ./bin/"${NAME}".o -o ./bin/"${NAME}"
+    rm ./bin/"${NAME}".o
+
+    ./bin/"${NAME}"
+fi
 
